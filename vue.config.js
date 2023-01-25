f96e38d4918a28e0c9384a2381cfea712a8e63aa
Andrey Gladyshev <andrey@uchi.pro>
2023-01-25 16:13:07 +0400

    build: собирать весь js в один файл

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    }
+  }
 })
